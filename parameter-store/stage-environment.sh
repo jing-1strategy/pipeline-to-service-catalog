480d0d8cf79d06b565d38a10dc4b6f2e8a836976
Jing Liang <jing@1strategy.com>
2019-06-19 15:23:34 -0700

    update file names

---
+++ parameter-store/stage-environment.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+aws ssm put-parameter \
+    --name "AmazonCloudWatch-Review-Portal-CW-Config" \
+    --type "String" \
+    --value "{
+	"logs": {
+		"logs_collected": {
+			"files": {
+				"collect_list": [
+					{
+						"file_path": "/var/log/nginx-access.log",
+						"log_group_name": "idbydna/review-portal-nginx-access-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					},
+					{
+						"file_path": "/var/log/nginx-error.log",
+						"log_group_name": "idbydna/review-portal-nginx-error-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					},
+					{
+						"file_path": "/var/log/review-portal-stdout.log",
+						"log_group_name": "idbydna/review-portal-stdout-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					},
+					{
+						"file_path": "/var/log/review-portal-stderr.log",
+						"log_group_name": "idbydna/review-portal-stderr-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					}
+				]
+			}
+		}
+	},
+	"metrics": {
+		"append_dimensions": {
+			"AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+			"ImageId": "${aws:ImageId}",
+			"InstanceId": "${aws:InstanceId}",
+			"InstanceType": "${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"cpu": {
+				"measurement": [
+					"cpu_usage_idle",
+					"cpu_usage_iowait",
+					"cpu_usage_user",
+					"cpu_usage_system"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				],
+				"totalcpu": false
+			},
+			"disk": {
+				"measurement": [
+					"used_percent",
+					"inodes_free"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				]
+			},
+			"diskio": {
+				"measurement": [
+					"io_time"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 30
+			},
+			"swap": {
+				"measurement": [
+					"swap_used_percent"
+				],
+				"metrics_collection_interval": 30
+			}
+		}
+	}
+}" \
+    --overwrite \
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "EC2KeyPairName" \
+    --type "String" \
+    --value "JingNV-Training" \
+    --overwrite
+
+aws ssm put-parameter \
+    --name "VPCId" \
+    --type "String" \
+    --value "vpc-0e89f4904fcf2e20c" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "VPCCidrBlock" \
+    --type "String" \
+    --value "10.64.0.0/16" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "PrivateSubnetList" \
+    --type "AWS::SSM::Parameter::Value<List<String>>" \
+    --value "subnet-0d9071e7b96a2fe75,subnet-07f42585e44249b68,subnet-0645b2222530ced5e" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "PublicSubnetList" \
+    --type "AWS::SSM::Parameter::Value<List<String>>" \
+    --value "subnet-0a7729a9fdebf9437,subnet-0e4c697665d6367ff,subnet-04c98816f0a1b928f" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "ExternalHostedZoneName" \
+    --type "String" \
+    --value "1strategy-training.com" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "ExternalRecordSetName" \
+    --type "String" \
+    --value "1s-jingprod" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "PrivateDNSHostedZoneName" \
+    --type "String" \
+    --value "internal.1strategy-training.com." \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "CertificateArn" \
+    --type "String" \
+    --value "arn:aws:acm:us-east-1:281782457076:certificate/6dcae0f8-28df-493e-8d57-f63ceded2ca1" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
---
+++ parameter-store/test-environment.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+aws ssm put-parameter \
+    --name "AmazonCloudWatch-Review-Portal-CW-Config" \
+    --type "String" \
+    --value "{
+	"logs": {
+		"logs_collected": {
+			"files": {
+				"collect_list": [
+					{
+						"file_path": "/var/log/nginx-access.log",
+						"log_group_name": "idbydna/review-portal-nginx-access-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					},
+					{
+						"file_path": "/var/log/nginx-error.log",
+						"log_group_name": "idbydna/review-portal-nginx-error-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					},
+					{
+						"file_path": "/var/log/review-portal-stdout.log",
+						"log_group_name": "idbydna/review-portal-stdout-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					},
+					{
+						"file_path": "/var/log/review-portal-stderr.log",
+						"log_group_name": "idbydna/review-portal-stderr-log",
+						"log_stream_name": "reviewportal-{instance_id}"
+					}
+				]
+			}
+		}
+	},
+	"metrics": {
+		"append_dimensions": {
+			"AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+			"ImageId": "${aws:ImageId}",
+			"InstanceId": "${aws:InstanceId}",
+			"InstanceType": "${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"cpu": {
+				"measurement": [
+					"cpu_usage_idle",
+					"cpu_usage_iowait",
+					"cpu_usage_user",
+					"cpu_usage_system"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				],
+				"totalcpu": false
+			},
+			"disk": {
+				"measurement": [
+					"used_percent",
+					"inodes_free"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				]
+			},
+			"diskio": {
+				"measurement": [
+					"io_time"
+				],
+				"metrics_collection_interval": 30,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 30
+			},
+			"swap": {
+				"measurement": [
+					"swap_used_percent"
+				],
+				"metrics_collection_interval": 30
+			}
+		}
+	}
+}" \
+    --overwrite \
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "EC2KeyPairName" \
+    --type "String" \
+    --value "JingNVKeyPair" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "VPCId" \
+    --type "String" \
+    --value "vpc-05ad25cd042facb61" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "VPCCidrBlock" \
+    --type "String" \
+    --value "10.64.0.0/16" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "PrivateSubnetList" \
+    --type "AWS::SSM::Parameter::Value<List<String>>" \
+    --value "subnet-0c1df95c81e4f0b48,subnet-0a7ff68fb72c66823,subnet-00b6b2301230ca831" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "PublicSubnetList" \
+    --type "AWS::SSM::Parameter::Value<List<String>>" \
+    --value "subnet-0fba18d11f5066df4,subnet-05fda5d3c1d844f7d,subnet-012a43b6b39f9233d" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "ExternalHostedZoneName" \
+    --type "String" \
+    --value "1strategy-sandbox.com" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "ExternalRecordSetName" \
+    --type "String" \
+    --value "1s-jingtest" \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "PrivateDNSHostedZoneName" \
+    --type "String" \
+    --value "internal.1strategy-sandbox.com." \
+    --overwrite
+    --profile $1 \
+    --region $2
+
+aws ssm put-parameter \
+    --name "CertificateArn" \
+    --type "String" \
+    --value "arn:aws:acm:us-east-1:842337631775:certificate/4c676396-4c8f-41fd-b5a3-7b71cf35adff" \
+    --overwrite
+    --profile $1 \
+    --region $2
